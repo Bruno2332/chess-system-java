eeba6ec1cd38b85dc24ff7f6fa1eaf878ad88a95
Bruno Flor de Lys <brunolys23@gmail.com>
2023-09-23 01:11:53 -0300

    Printing possible moves

--- src/chess/pieces/Rook.java
+++ src/chess/pieces/Rook.java
@@ -18,7 +18,7 @@ public class Rook extends ChessPiece {
 	}
 
 	@Override
-	public boolean[][] possibleMove() {
+	public boolean[][] possibleMoves() {
 		boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getRows()];
 		
 		// above
